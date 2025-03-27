0436544144b625ad40f819964aec8ec880f0f829
ANSHUL GOLA <anshulgola5339@gmail.com>
2025-03-27 19:15:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+ScrollReveal().reveal('.headline');
+
+b=document.getElementById("bdy1")
+b.addEventListener
+(
+    "click",
+    function()
+    {
+        audio=document.getElementById("audi")
+        audio.play()
+    }
+)
